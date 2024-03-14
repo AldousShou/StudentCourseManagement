c68664d693969eb70ebb844583b61feefdb4a507
AldousShou <1216614219@qq.com>
2024-03-14 17:34:41 +0800

    修改 rabbit 的 config 配置方式

--- Core/src/main/java/com/shoumh/core/configuration/RabbitMQConfig.java
+++ Core/src/main/java/com/shoumh/core/configuration/RabbitMQConfig.java
@@ -5,6 +5,7 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,18 +13,19 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfig {
 
+    private static final Jackson2JsonMessageConverter jackson2JsonMessageConverter = new Jackson2JsonMessageConverter();
+    private static final SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+
     @Bean
     public MessageConverter messageConverter(){
-        return new Jackson2JsonMessageConverter();
+        return jackson2JsonMessageConverter;
     }
 
     @Bean
     public RabbitListenerContainerFactory<?> rabbitListenerContainerFactory(ConnectionFactory connectionFactory){
-        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
         factory.setConnectionFactory(connectionFactory);
         factory.setMessageConverter(new Jackson2JsonMessageConverter());
         return factory;
     }
-
 
 }
